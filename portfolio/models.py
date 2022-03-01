13da527c81f852c6cffb565ae2b7700f365ae0cd
Matt Snell <msnell@unomaha.edu>
2022-03-01 10:09:12 -0600

    Lab 3 Updates 8380

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+import requests
 
 # Create your models here.
 class Customer(models.Model):
@@ -68,3 +68,23 @@ class Stock(models.Model):
 
     def initial_stock_value(self):
         return self.shares * self.purchase_price
+
+    def current_stock_price(self):
+            symbol_f = str(self.symbol)
+            main_api = 'http://api.marketstack.com/v1/eod?'
+            api_key = 'access_key=c6a1e9966e3668749e948cfa0c5ca020&limit=1&symbols='
+            url = main_api + api_key + symbol_f
+            json_data = requests.get(url).json()
+            open_price = float(json_data["data"][0]["open"])
+            share_value = open_price
+            return share_value 
+
+    def current_stock_value(self):
+        return float(self.current_stock_price()) * float(self.shares)
+    
+    def results_by_stock(self):
+        now = self.current_stock_value
+        then = self.initial_stock_value
+        return now - then
+
+
